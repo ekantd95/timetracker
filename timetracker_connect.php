43147956d3bf4b9a2fe866b1a68aa63a5599be23
ekantd95 <ekantd@gmail.com>
2018-08-29 13:52:37 -0500

    Update timetracker_connect.php

--- timetracker_connect.php
+++ timetracker_connect.php
@@ -1,10 +1,10 @@
 <?php # indextyper_connect.php
 
   // Set credentials
-  DEFINE ('DB_USER', 'ekantd95_first');
-  DEFINE ('DB_PASSWORD', 'fishtacos');
-  DEFINE ('DB_HOST', 'localhost');
-  DEFINE ('DB_NAME', 'ekantd95_timetracker');
+  DEFINE ('DB_USER', '');
+  DEFINE ('DB_PASSWORD', '');
+  DEFINE ('DB_HOST', '');
+  DEFINE ('DB_NAME', '');
 
   // Establish connection
   $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die('Could not connect to MySql: ' . mysqli_connect_error() );
